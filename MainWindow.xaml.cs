7fa41ec9e633ff32d001ccc67df663c7a7448a79
Kenneth M <12839703+Minmoose@users.noreply.github.com>
2022-09-22 16:18:44 -0500

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.UI.Xaml;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using WinUIEx;
+
+namespace FFmpegInteropX_WinUI_UnpackagedDemo;
+
+public sealed partial class MainWindow : Window
+{
+    readonly Windows.Media.Playback.MediaPlayer mediaPlayer = new();
+    FFmpegInteropX.FFmpegMediaSource mediaSource;
+    Windows.Storage.Streams.IRandomAccessStream stream;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private async void Button_Click(object sender, RoutedEventArgs e)
+    {
+        var picker = new FileOpenPicker();
+        picker.ViewMode = PickerViewMode.Thumbnail;
+        picker.SuggestedStartLocation = PickerLocationId.MusicLibrary;
+        picker.FileTypeFilter.Add("*");
+
+        WinRT.Interop.InitializeWithWindow.Initialize(picker, this.GetWindowHandle());
+
+        var file = await picker.PickSingleFileAsync();
+
+        if (file != null)
+        {
+            stream = await file.OpenAsync(FileAccessMode.Read);
+
+            mediaSource = await FFmpegInteropX.FFmpegMediaSource.CreateFromStreamAsync(stream);
+            mediaPlayer.Source = mediaSource.CreateMediaPlaybackItem();
+
+            mp.SetMediaPlayer(mediaPlayer);
+
+            mediaPlayer.Play();
+        }
+    }
+}
